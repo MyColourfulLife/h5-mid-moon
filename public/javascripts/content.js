79f989d95313cf7c6fb2a170145d5dba91a2154b
myemail <myemail@emal.com>
2017-09-28 14:00:49 +0800

    调整了板式，弹幕还是有问题

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -52,39 +52,62 @@ var app = new Vue({
   methods: {
     click_moon: function(event) {
       // this 指当前 app实例 就是上面 var app = 。。。
-      alert("you click me!!!");
+      console.log('到下一页');
+      $('#page2').attr('hidden',false);
+      $('#page2').addClass('animated bounceInUp');
     },
     click_crama: function(event) {
-      alert("wanna open carma");
+      console.log("wanna open carma");
+      $('#page3').attr('hidden',false);
+      $('#page3').addClass('animated bounceInUp');
     },
     click_send: function(event) {
-      alert("点我发送");
+      console.log("点我发送");
+      $('#page4').attr('hidden',false);
+      $('#page4').addClass('animated bounceInUp');
     },
     get_allwords: function() {},
     startDm: function() {
-      var danmu = document.getElementById(`danmu`);
-      var winH = danmu.clientHeight;
+      console.log($('#danmu'));
+      // var danmu = document.getElementById(`danmu`);
+      // var winH = danmu.clientHeight;
+
       var speak = this.dmlists;
+      var toplist = [25,50,75,100];
       function insert() {
+       var danmu = document.getElementById(`danmu`);
+      var winH = danmu.clientHeight;
+        console.log('高度',winH);
         var newli = document.createElement(`div`);
         var randomS = Math.floor(Math.random() * speak.length);
-        var r = Math.floor(Math.random() * 266);
-        var g = Math.floor(Math.random() * 266);
-        var b = Math.floor(Math.random() * 266);
-
         var pserson = speak[randomS];
         var content = `<span> <img src=${pserson.avator}> ${pserson.nickname}:${pserson.words} </span>`;
         newli.innerHTML = content;
         newli.className = `newli`;
-        newli.style.color = `rgb(${r}, ${g} ,${b})`;
+        newli.style.color = '#000000'
         var t = winH - 30;
         var newliT = Math.floor(Math.random() * (t - 1));
+        console.log(`高${winH},t:${t},top:${newliT}`);
         var newliL = danmu.clientWidth;
         newli.style.left = newliL + `px`;
         newli.style.top = newliT + `px`;
+        // var topIndex = getRandomIntInclusive(0,toplist.length - 1);
+        // console.log(toplist[topIndex]);
+        // newli.style.top = toplist[topIndex] *  + `px`;
         danmu.appendChild(newli);
         move(newli);
       }
+
+      /**
+ *生成随机数的函数 
+ *获取 min max 之间 的随机整数
+ */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
       function move(obj) {
         var timer1 = setInterval(function() {
           // var danmu=document.getElementById(`danmu`);
@@ -96,20 +119,22 @@ var app = new Vue({
             clearInterval(timer1);
           }
           obj.style.left = run + `px`;
-        }, 1);
+        }, 10);
       }
       // insert();
       var timer2 = setInterval(function() {
         insert();
-      }, 1000);
+      }, 5000);
       window.onfocus = function() {
         clearInterval(timer2);
+        timer2 = null;
         timer2 = setInterval(function() {
           insert();
         }, 1000);
       };
       window.onblur = function() {
         clearInterval(timer2);
+        timer2 = null;
       };
     }
   }
