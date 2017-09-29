ed0fc95d98a984959718a57d6b1da4f558090217
myemail <myemail@emal.com>
2017-09-29 21:59:40 +0800

    图片上传接口

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -1,5 +1,3 @@
-
-
 // 参数配置
 var images = [];
 for (var i = 1; i <= 4; i++) {
@@ -46,7 +44,8 @@ var app = new Vue({
         words: `
         明月装饰了你的窗子,
         你装饰了别人的梦.`
-      },{
+      },
+      {
         nickname: "瘾君子",
         avator: "images/sharedavator.jpg",
         words: `
@@ -57,94 +56,91 @@ var app = new Vue({
   methods: {
     click_moon: function(event) {
       // this 指当前 app实例 就是上面 var app = 。。。
-      console.log('到下一页');
-      $('#page2').attr('hidden',false);
-      $('#page2').addClass('animated slideInUp');
+      console.log("到下一页");
+      $("#page2").attr("hidden", false);
+      $("#page2").addClass("animated slideInUp");
     },
     click_crama: function(event) {
       console.log("wanna open carma");
-      $('#page3').attr('hidden',false);
-     
+      $("#page3").attr("hidden", false);
 
       // 1. 获取图像
 
       wx.chooseImage({
         count: 1, // 默认9
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function (res) {
-        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+        sizeType: ["compressed"], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ["camera"], // 可以指定来源是相册还是相机，默认二者都有
+        success: function(res) {
+          console.log("拍照结果:", res);
+          
+          var localIds = res.localIds[0]; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+          // 2. 拿到图片 将图片上传
+          wx.uploadImage({
+            localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+            isShowProgressTips: 1, // 默认为1，显示进度提示
+            success: function(res) {
+              var serverId = res.serverId; // 返回图片的服务器端ID
 
-      // 2. 拿到图片 将图片上传
-      wx.uploadImage({
-        localId: '', // 需要上传的图片的本地ID，由chooseImage接口获得
-        isShowProgressTips: 1, // 默认为1，显示进度提示
-        success: function (res) {
-            var serverId = res.serverId; // 返回图片的服务器端ID
+              var imgurl = `http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=${wxglobleconfig.token}&media_id=${serverId}`
 
+              window.location.href=imgurl;
+              // 3. 图像识别服务器验证 也可能是 服务器短 验证
 
-            // 3. 图像识别服务器验证 也可能是 服务器短 验证
-
-            // 如果拿到了图片地址 请求服务器验证
-            axios.get().then(
-              // 如果成功，
-             function (res) {
-              //  1. 显示下一页 需要获取的数据 包括 别人的话 弹幕 等 网页加载完就先请求，到时候只是显示数据
-              $('#page3').addClass('animated slideInUp');
-
-             }
-
-
-            ).catch();
-
-
-
-        }
-    });
-
-
+              // 如果拿到了图片地址 请求服务器验证
+              axios
+                .get()
+                .then(
+                  // 如果成功，
+                  function(res) {
+                    //  1. 显示下一页 需要获取的数据 包括 别人的话 弹幕 等 网页加载完就先请求，到时候只是显示数据
+                    $("#page3").addClass("animated slideInUp");
+                  }
+                )
+                .catch();
+            }
+          });
         },
-        fail:function(err){
-          console.log(err);
+        fail: function(err) {
+          console.log('拍照出错:',err);
         }
-    });
+      });
     },
     click_send: function(event) {
       console.log("点我发送");
-      $('#page4').attr('hidden',false);
-      $('#page4').addClass('animated slideInUp');
+      $("#page4").attr("hidden", false);
+      $("#page4").addClass("animated slideInUp");
 
       // donmutwo();
       // var timer = setInterval(donmutwo,1000);
     },
     get_allwords: function() {},
     startDm: function() {
-      console.log($('#danmu'));
+      console.log($("#danmu"));
       // var danmu = document.getElementById(`danmu`);
       // var winH = danmu.clientHeight;
 
       var speak = this.dmlists;
-     
+
       function insert() {
-       var danmu = document.getElementById(`danmu`);
-      var winH = danmu.clientHeight;
-      var toplist = [0.1 * winH,0.3 * winH,0.5 * winH,0.7 * winH];
+        var danmu = document.getElementById(`danmu`);
+        var winH = danmu.clientHeight;
+        var toplist = [0.1 * winH, 0.3 * winH, 0.5 * winH, 0.7 * winH];
         var newli = document.createElement(`div`);
         var randomS = Math.floor(Math.random() * speak.length);
         var pserson = speak[randomS];
         var content = `<span> <img src=${pserson.avator}> ${pserson.nickname}:${pserson.words} </span>`;
         newli.innerHTML = content;
         newli.className = `newli`;
-        newli.style.color = '#000000'
+        newli.style.color = "#000000";
         var t = winH - 30;
         var newliT = Math.floor(Math.random() * (t - 1));
         console.log(`高${winH},t:${t},top:${newliT}`);
         var newliL = danmu.clientWidth;
         newli.style.left = newliL + `px`;
         // newli.style.top = newliT + `px`;
-        var topIndex = getRandomIntInclusive(0,toplist.length - 1);
+        var topIndex = getRandomIntInclusive(0, toplist.length - 1);
         console.log(toplist);
-        console.log(topIndex,toplist[topIndex]);
+        console.log(topIndex, toplist[topIndex]);
         newli.style.top = toplist[topIndex] + `px`;
         danmu.appendChild(newli);
         move(newli);
@@ -154,14 +150,14 @@ var app = new Vue({
  *生成随机数的函数 
  *获取 min max 之间 的随机整数
  */
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
+      function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+      }
 
       function move(obj) {
-      var timer1 = setInterval(function() {
+        var timer1 = setInterval(function() {
           // var danmu=document.getElementById(`danmu`);
           var newliW = obj.clientWidth;
           var run = obj.offsetLeft;
@@ -194,38 +190,31 @@ function getRandomIntInclusive(min, max) {
 
 // app.startDm();
 
-
-
-
-
-
 function donmutwo() {
-   $("danmu").danmu({
-    height: 200,  //弹幕区高度
-    width: 640,   //弹幕区宽度
-    zindex :100,   //弹幕区域z-index属性
-    speed:10000,      //滚动弹幕的默认速度，这是数值值得是弹幕滚过每672像素所需要的时间（毫秒）
-    sumTime:65535,   //弹幕流的总时间
-    danmuLoop:true,   //是否循环播放弹幕
-    defaultFontColor:"#FFFFFF",   //弹幕的默认颜色
-    fontSizeSmall:16,     //小弹幕的字号大小
-    FontSizeBig:24,       //大弹幕的字号大小
-    opacity:"0.9",			//默认弹幕透明度
-    topBottonDanmuTime:6000,   // 顶部底部弹幕持续时间（毫秒）
-    SubtitleProtection:false,     //是否字幕保护
-    positionOptimize:false,         //是否位置优化，位置优化是指像AB站那样弹幕主要漂浮于区域上半部分
-    
-    maxCountInScreen: 6,   //屏幕上的最大的显示弹幕数目,弹幕数量过多时,优先加载最新的。
-    maxCountPerSec: 10,      //每分秒钟最多的弹幕数目,弹幕数量过多时,优先加载最新的。
-    });
-  $("#danmu").danmu("addDanmu",[
-    { text:"巴拉" ,color:"black",size:1,position:0,time: 1},
-    { text:"jerk" ,color:"black",size:1,position:0,time:10},
-    { text:"yami" ,color:"black",size:1,position:0,time:20}
- ])
- $('#danmu').danmu('danmuStart');
+  $("danmu").danmu({
+    height: 200, //弹幕区高度
+    width: 640, //弹幕区宽度
+    zindex: 100, //弹幕区域z-index属性
+    speed: 10000, //滚动弹幕的默认速度，这是数值值得是弹幕滚过每672像素所需要的时间（毫秒）
+    sumTime: 65535, //弹幕流的总时间
+    danmuLoop: true, //是否循环播放弹幕
+    defaultFontColor: "#FFFFFF", //弹幕的默认颜色
+    fontSizeSmall: 16, //小弹幕的字号大小
+    FontSizeBig: 24, //大弹幕的字号大小
+    opacity: "0.9", //默认弹幕透明度
+    topBottonDanmuTime: 6000, // 顶部底部弹幕持续时间（毫秒）
+    SubtitleProtection: false, //是否字幕保护
+    positionOptimize: false, //是否位置优化，位置优化是指像AB站那样弹幕主要漂浮于区域上半部分
+
+    maxCountInScreen: 6, //屏幕上的最大的显示弹幕数目,弹幕数量过多时,优先加载最新的。
+    maxCountPerSec: 10 //每分秒钟最多的弹幕数目,弹幕数量过多时,优先加载最新的。
+  });
+  $("#danmu").danmu("addDanmu", [
+    { text: "巴拉", color: "black", size: 1, position: 0, time: 1 },
+    { text: "jerk", color: "black", size: 1, position: 0, time: 10 },
+    { text: "yami", color: "black", size: 1, position: 0, time: 20 }
+  ]);
+  $("#danmu").danmu("danmuStart");
 }
 
 donmutwo();
-
-
