c41357c2f5e49e7ac4d87a98263dc4c5fcd08fe6
myemail <myemail@emal.com>
2017-09-29 21:07:56 +0800

    解决微信签名的问题

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+
+app.use('/moon', index);
 app.use('/users', users);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var get_config = require("../serverjs/check_sign.js");
+
+router.use(function (req,res,next) {
+  console.log('收到请求',req.url);
+  next();
+});
+
+router.get('/wechatconfig',function (req,res) {
+// 获取微信注册配置信息
+  var requesturl = req.url;
+ var configurl = requesturl.split("xxconfigurl=")[1].split('#')[0];
+  var config = get_config(configurl);
+  res.send(config);
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  //创建渲染模型
-
-  var {datamodel} = {title:'明月知我心'}
 
 // 默认数据
   var avator = "images/sharedavator.jpg";
   var words = "都云作者痴，谁解其中味";
   var nickname = "山巅思";
+
+  //微信服务器校验
+  if (req.query.echostr) {
+    console.log('收到微信校验',req.query);
+    res.send(req.query.echostr);
+    return;
+  }
+
 
   // 获取用户参数
   if (req.query.nickname) {
