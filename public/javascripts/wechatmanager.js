cced5407f2a75a89fcb982d00fb18dd4d40216b5
myemail <myemail@emal.com>
2017-09-30 20:13:17 +0800

    测试分享接口

--- public/javascripts/wechatmanager.js
+++ public/javascripts/wechatmanager.js
@@ -19,7 +19,7 @@ axios
       wx.onMenuShareAppMessage({
         title: "我偷偷的告诉你", // 分享标题
         desc: "从月亮里发现一颗心", // 分享描述
-        link: `http://www.id-bear.com/node/moon/moon?saytomoon=${app.data.inputMessage}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        link: `http://www.id-bear.com/node/moon/moon?saytomoon=${app.inputMessage}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
         imgUrl: "images/sharedimg.jpg", // 分享图标
         type: "link", // 分享类型,music、video或link，不填默认为link
         dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
@@ -33,7 +33,7 @@ axios
 
       wx.onMenuShareTimeline({
         title: "我把话都放月亮里，进来看看吧", // 分享标题
-        link: `http://www.id-bear.com/node/moon/moon?saytomoon=${app.data.inputMessage}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        link: `http://www.id-bear.com/node/moon/moon?saytomoon=${app.inputMessage}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
         imgUrl: "images/sharedimg.jpg", // 分享图标
         success: function() {
           // 用户确认分享后执行的回调函数
