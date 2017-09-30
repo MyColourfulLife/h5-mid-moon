8728aa88e42331a9c997c75766fb77dd8be7b8a8
myemail <myemail@emal.com>
2017-09-30 19:02:39 +0800

    djafd

--- public/javascripts/wechatmanager.js
+++ public/javascripts/wechatmanager.js
@@ -44,11 +44,22 @@ axios
 
     wx.ready(function() {
 
-
-
-
-
-
+       // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
+      wx.onMenuShareAppMessage({
+        title: "ooooo", // 分享标题
+        desc: "1111", // 分享描述
+        link: "http://www.id-bear.com/node/moon/asdfadafsdasd", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "", // 分享图标
+        type: "link", // 分享类型,music、video或link，不填默认为link
+        dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
+        success: function() {
+          // 用户确认分享后执行的回调函
+          alert("sb");
+        },
+        cancel: function() {
+          // 用户取消分享后执行的回调函数
+        }
+      });
 
     });
 
@@ -63,23 +74,7 @@ axios
 
 
   
-  // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
-  wx.onMenuShareAppMessage({
-    title: "ooooo", // 分享标题
-    desc: "1111", // 分享描述
-    link: "http://www.id-bear.com/node/moon/moon?name=hello", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: "", // 分享图标
-    type: "link", // 分享类型,music、video或link，不填默认为link
-    dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
-    success: function() {
-      // 用户确认分享后执行的回调函数
-      console.log();
-      alert();
-    },
-    cancel: function() {
-      // 用户取消分享后执行的回调函数
-    }
-  });
+ 
   
   
   // 上传图片接口
