475fff714f1ddb427a3ec11fd806f1485710b9ce
myemail <myemail@emal.com>
2017-10-01 01:47:40 +0800

    增加音乐

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -4,13 +4,11 @@ for (var i = 1; i <= 4; i++) {
   images.push(`./images/0${i}.png`);
 }
 
-
- 
 // vue
 var app = new Vue({
   el: "#app",
   data: {
-    inputMessage:null
+    inputMessage: null
   },
   methods: {
     click_moon: function(event) {
@@ -19,29 +17,57 @@ var app = new Vue({
       $("#page2").attr("hidden", false);
       $("#page2").addClass("animated slideInUp");
     },
-    shoot:function () {
+    shoot: function() {
       //发射弹幕
       if (!this.inputMessage || this.inputMessage.length === 0) {
         return;
       }
-      $('.danmuArea').barrager(createDanmuItem(this.inputMessage,"avatorimgs/avator66.jpg",30,'red',0));
+      $(".danmuArea").barrager(
+        createDanmuItem(
+          this.inputMessage,
+          "avatorimgs/avator66.jpg",
+          30,
+          "red",
+          0
+        )
+      );
 
-        window.location.href = `http://www.id-bear.com/node/moon/moon?saytomoon=${this.inputMessage}`;
+      window.location.href = `http://www.id-bear.com/node/moon/moon?saytomoon=${this
+        .inputMessage}`;
+    },
+    playstop: function(event) {
+      // 音乐
+      var audio = document.getElementById("bgMusic");
 
-
+      var isPlaying = audio.currentTime > 0 && !audio.paused && !audio.ended 
+      && audio.readyState > 2;
+  
+  if (!isPlaying) {
+    audio.play();
+    $('#playstop').removeClass("glyphicon glyphicon-volume-off");
+    $('#playstop').addClass('glyphicon glyphicon-volume-up');
+  } else {
+    audio.pause();
+    $('#playstop').removeClass("glyphicon glyphicon-volume-up");
+    $('#playstop').addClass('glyphicon glyphicon-volume-off');
+  }
+        // if(audio.paused){ 
+        //   audio.play();
+        //   $('#playstop').removeClass("glyphicon glyphicon-volume-off");
+        //   $('#playstop').addClass('glyphicon glyphicon-volume-up');
+        // }else{
+        //   audio.pause();
+        //   $('#playstop').removeClass("glyphicon glyphicon-volume-up");
+        //   $('#playstop').addClass('glyphicon glyphicon-volume-off');
+        // }
     },
     click_crama: function(event) {
-     
-      
-
       // 1. 获取图像
-
       wx.chooseImage({
         count: 1, // 默认9
         sizeType: ["compressed"], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ["camera"], // 可以指定来源是相册还是相机，默认二者都有
         success: function(res) {
-
           var localIds = res.localIds[0]; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
           // 2. 拿到图片 将图片上传
           wx.uploadImage({
@@ -88,23 +114,19 @@ var app = new Vue({
       console.log("点我发送");
       $("#page4").attr("hidden", false);
       $("#page4").addClass("animated slideInUp");
-
     }
   }
 });
-
-
 
 var saytomoon = "";
 window.saytomoon = saytomoon;
 
 function show() {
-
   window.saytomoon = app.inputMessage;
   wx.onMenuShareAppMessage({
     title: "我偷偷的告诉你", // 分享标题
     desc: "月亮里有我说的话", // 分享描述
-    link: window.location.href , // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
     imgUrl: "images/sharedimg.jpg", // 分享图标
     type: "link", // 分享类型,music、video或link，不填默认为link
     dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
@@ -113,26 +135,24 @@ function show() {
     },
     cancel: function() {
       // 用户取消分享后执行的回调函数
-            alert('您取消了分享');
+      alert("您取消了分享");
     },
-    trigger: function (res) {
+    trigger: function(res) {
       // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-      alert("点击分享朋友"+res.data);
-  },
+      alert("点击分享朋友" + res.data);
+    }
   });
-
 
   wx.onMenuShareTimeline({
     title: "我把话都放月亮里，进来看看吧", // 分享标题
-    link: window.location.href , // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
     imgUrl: "images/sharedimg.jpg", // 分享图标
     success: function() {
       // 用户确认分享后执行的回调函数
     },
     cancel: function() {
       // 用户取消分享后执行的回调函数
-      alert('您取消了分享');
+      alert("您取消了分享");
     }
   });
-
 }
